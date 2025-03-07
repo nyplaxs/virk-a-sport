2a710a3b1d73505d9a84dafe7a4a12d0378f0dc1
nyplaxs <nyplaxs@gmail.com>
2025-03-07 09:36:11 +0100

    Update firebaseConfig.js

--- vitalxreact/firebaseConfig.js
+++ vitalxreact/firebaseConfig.js
@@ -2,12 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyAp0RqbD6zGCIa2TBVToZrgKM18tZlXTiY",
+  projectId: "virk-a",
+  storageBucket: "virk-a.firebasestorage.app",
+  messagingSenderId: "952403294314",
+  appId: "1:952403294314:android:ed4df85e1cbdc0ae1ab213"
 };
 
 // Initialiser Firebase
