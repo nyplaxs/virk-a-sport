a915e4fcb077179fb1bc41e7b79b6d41e2988e84
nyplaxs <nyplaxs@gmail.com>
2025-03-14 09:04:49 +0100

    Update ChatListScreen.js

--- vitalxreact/src/screens/ChatListScreen.js
+++ vitalxreact/src/screens/ChatListScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { View, FlatList, Text, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, FlatList, Text, ActivityIndicator, RefreshControl } from 'react-native';
 import { getChatList } from '../api/chatApi';
 import ChatItem from '../components/ChatItem';
 import styles from '../styles/globalStyles';
@@ -7,38 +7,54 @@ import styles from '../styles/globalStyles';
 const ChatListScreen = ({ navigation }) => {
   const [chats, setChats] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);  // Nouvel état pour gérer les erreurs
+  const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchChats = async () => {
+    try {
+      setIsLoading(true);
+      const data = await getChatList();
+      setChats(data);
+      setError(null);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des chats : ", error);
+      setError("Impossible de charger les chats. Veuillez réessayer plus tard.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchChats = async () => {
-      try {
-        const data = await getChatList();
-        setChats(data);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des chats : ", error);
-        setError("Impossible de charger les chats. Veuillez réessayer plus tard.");
-      } finally {
-        setIsLoading(false);
-      }
-    };
     fetchChats();
+  }, []);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchChats();
+    setRefreshing(false);
   }, []);
 
   return (
     <View style={styles.container}>
       {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" />  // Indicateur de chargement
+        <ActivityIndicator size="large" color="#0000ff" />
       ) : error ? (
-        <Text>{error}</Text>  // Afficher un message d'erreur si quelque chose va mal
+        <Text>{error}</Text>
       ) : chats.length === 0 ? (
-        <Text style={styles.noChatsText}>Pas encore de discussions. Commencez à discuter avec vos contacts !</Text>  // Nouveau message
+        <Text style={styles.noChatsText}>Pas encore de discussions. Commencez à discuter avec vos contacts !</Text>
       ) : (
         <FlatList
           data={chats}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
-            <ChatItem chat={item} onPress={() => navigation.navigate('MessageScreen', { chatId: item.id })} />
+            <ChatItem
+              chat={item}
+              onPress={() => navigation.navigate('MessageScreen', { chatId: item.id })}
+            />
           )}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         />
       )}
     </View>
